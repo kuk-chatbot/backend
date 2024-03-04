12cc621564159e95d95a0770944a59e71d896c7f
bakukun <h1101201@gmail.com>
2024-03-04 16:53:13 +0900

    테이블 매핑 (2024-03-04)

--- src/main/java/com/oosulz/blog/model/Board.java
+++ src/main/java/com/oosulz/blog/model/Board.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.security.Timestamp;
+import java.util.List;
 
 
 @Data
@@ -31,13 +32,16 @@ public class Board {
     @CreationTimestamp //자동입력
     private Timestamp createDate;
 
-    @ManyToOne //보드 다 : user 1
+    @ManyToOne(fetch = FetchType.EAGER) //보드 다 : user 1 (1건 밖에 없으니 바로 조인)
     @JoinColumn(name="userId")
     private User user;
     // DB는 오브젝트 저장 X
     // FK, 자바는 오브젝트를 저장 할 수 있다.
     // ORM 쓰면 DB도 오브젝트 저장 가능
-
+    @OneToMany(mappedBy = "board",fetch = FetchType.EAGER)
+    // mappedBy = 연관관계 주인 아니다 = 난 FK 아니에여 / DB컬럼 만들지 마라
+    //
+    private List<Reply> reply;
 
 
 }
