090e83f1a48f6ea9ff24181c511dd299be264dcf
MinJae00 <exceedmin@gmail.com>
2024-05-23 00:37:32 +0900

    user_info

--- src/main/java/com/kuk/chatbot/api/DashboardApiController.java
+++ src/main/java/com/kuk/chatbot/api/DashboardApiController.java
@@ -2,10 +2,15 @@ package com.kuk.chatbot.api;
 
 import com.kuk.chatbot.dto.ResponseDto;
 import com.kuk.chatbot.model.User;
+import com.kuk.chatbot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -13,10 +18,12 @@ import java.security.Principal;
 @RestController
 public class DashboardApiController {
 
-    @GetMapping("/dashboard/account")
-    public ResponseDto<Integer> save(@RequestBody User user){ //username, password, email
 
-        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
-    }
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
 
 }
--- src/main/java/com/kuk/chatbot/api/UserApiController.java
+++ src/main/java/com/kuk/chatbot/api/UserApiController.java
@@ -5,11 +5,14 @@ import com.kuk.chatbot.model.User;
 import com.kuk.chatbot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 public class UserApiController {
@@ -20,15 +23,13 @@ public class UserApiController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user){ //username, password, email
         System.out.println("UserApiController save 호출");
         userService.회원가입(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
-
-
-
 
     //기본 로그인 (security 적용x)
     /*
@@ -54,5 +55,25 @@ public class UserApiController {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+    }
+
+    @GetMapping("/summary")
+    public ResponseEntity<ResponseDto<User>> getUserDashboardData(Principal principal) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username = authentication.getName();
+
+        User user = userService.회원찾기(username);
+
+        if (user != null) {
+            // 사용자 정보를 담은 ResponseDto 객체를 생성합니다.
+            ResponseDto<User> response = new ResponseDto<>(HttpStatus.OK.value(), user);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            // 사용자를 찾을 수 없는 경우
+            ResponseDto<User> response = new ResponseDto<>(HttpStatus.NOT_FOUND.value(), null);
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
     }
 }
