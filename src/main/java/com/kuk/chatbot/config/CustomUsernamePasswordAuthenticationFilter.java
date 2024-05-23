1ea77d96ecf0a6dd1b6cbb45aaacfdccd61e50a4
bakukun <h1101201@gmail.com>
2024-05-23 22:12:04 +0900

    react 연동

---
+++ src/main/java/com/kuk/chatbot/config/CustomUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package com.kuk.chatbot.config;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public CustomUsernamePasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        if ("application/x-www-form-urlencoded".equals(request.getContentType())) {
+            String body;
+            try {
+                body = request.getReader().lines().collect(Collectors.joining());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            String[] params = body.split("&");
+            String username = null;
+            String password = null;
+
+            for (String param : params) {
+                String[] keyValue = param.split("=");
+                String key = URLDecoder.decode(keyValue[0], StandardCharsets.UTF_8);
+                String value = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
+                if ("username".equals(key)) {
+                    username = value;
+                } else if ("password".equals(key)) {
+                    password = value;
+                }
+            }
+
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+            setDetails(request, authRequest);
+            return this.getAuthenticationManager().authenticate(authRequest);
+        }
+
+        return super.attemptAuthentication(request, response);
+    }
+}
